f4e24e6059652aaf26b230b5b73ad212277a4d2a
Tapio Jokinen <tapio.jokinen@hotmail.com>
2025-01-25 23:16:37 +0200

    Fix run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-# Start Jenkins
-cd ~/ProductionServer/Jenkins
-
-docker compose up -d
-
 # Start NGINX
 
 cd ~/ProductionServer/NGINX
 
 docker compose up --build -d
+
+# Start Jenkins
+cd ~/ProductionServer/Jenkins
+
+docker compose up -d
