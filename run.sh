a12df98164a095d4d35ff28fe3889b381f6bf915
Tapio Jokinen <tapio.jokinen@hotmail.com>
2025-01-26 15:32:09 +0200

    Fix run.sh

--- run.sh
+++ run.sh
@@ -9,11 +9,3 @@ docker compose up --build -d
 cd ~/ProductionServer/Jenkins
 
 docker compose up -d
-
-JENKINS_IP=$(sudo docker inspect $(sudo docker ps | grep jenkins_docker | awk '{print $1}') | jq -r '.[0].NetworkSettings.Networks[].IPAddress')
-
-sudo ufw deny from all to 22
-
-sudo ufw allow from 192.168.0.0/24 to 22
-
-sudo ufw allow from "$JENKINS_IP" to 22
